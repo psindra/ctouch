77715103013f89ed98c565691faef8fd929e294a
psindra <45101133+psindra@users.noreply.github.com>
2020-09-03 18:38:46 -0300

    Update undisposition_bg.js

--- undisposition/undisposition_bg.js
+++ undisposition/undisposition_bg.js
@@ -1,24 +1,26 @@
 var active = true;
-
 chrome.webRequest.onHeadersReceived.addListener(
 	function(details){
-	console.log(details);
+	//console.log(details);
 		var headers=details.responseHeaders;
-		for(var i=0;i<headers.length;i++){
-			if(headers[i].name.toLowerCase()=='content-disposition'){
-				if(headers[i].value.indexOf('attachment')==0){
-					headers.splice(i,1);
-					//headers[i].value='inline';
+		if (active)
+		{
+			for(var i=0;i<headers.length;i++){
+				if(headers[i].name.toLowerCase()=='content-disposition'){
+					if(headers[i].value.indexOf('attachment')==0){
+						headers.splice(i,1);
+						//headers[i].value='inline';
+					}
+					break;
 				}
-				break;
 			}
-		}
-		for(var j=0;j<headers.length;j++){
-			if(headers[j].name.toLowerCase()=='content-type'){
-				if(headers[j].value=='application/octet-stream'||headers[j].value=='application/x-download'){
-					headers[j].value='text/plain'; //I hope Chrome is wise enough
+			for(var j=0;j<headers.length;j++){
+				if(headers[j].name.toLowerCase()=='content-type'){
+					if(headers[j].value=='application/octet-stream'||headers[j].value=='application/x-download'){
+						headers[j].value='text/plain'; //I hope Chrome is wise enough
+					}
+					break;
 				}
-				break;
 			}
 		}
 		return {responseHeaders: headers};
@@ -29,7 +31,6 @@ chrome.webRequest.onHeadersReceived.addListener(
 	},
 	['blocking', 'responseHeaders']
 );
-
 
 function loadOptions(callback)
 {
